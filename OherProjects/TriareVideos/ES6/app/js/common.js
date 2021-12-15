5052e1484e55474157a6e078d232e7b8758e3698
Perforator_lubvi <korz372@gmail.com>
2021-12-15 23:03:58 +0300

    added for... of

--- OherProjects/TriareVideos/ES6/app/js/common.js
+++ OherProjects/TriareVideos/ES6/app/js/common.js
@@ -1,49 +1,65 @@
-let a=[1,2,3,4];
-let b=a.filter((num)=>num>3);
-console.log(b);
-a.push(5);
-console.log(a);
+// let a=[1,2,3,4];
+// let b=a.filter((num)=>num>3);
+// console.log(b);
+// a.push(5);
+// console.log(a);
 
 
-const Obj={
-    es:"ES6"
-};
-Obj.es='ES7';
-console.log(Obj);
+// const Obj={
+//     es:"ES6"
+// };
+// Obj.es='ES7';
+// console.log(Obj);
 
-const num1=[1,2,3,4];
-const num2=[5,6,7,8];
-console.log([...num1, ...num2]);
+//Spread REST
+// const num1=[1,2,3,4];
+// const num2=[5,6,7,8];
+// console.log([...num1, ...num2]);
+//
+// const A={
+//     a:'default',
+//     b:'just test'
+// }
+// const B={
+//     a:'value from b'
+// }
+// let C= {
+//     ...A,
+//     ...B
+// }
+//
+// console.log(C);
+// const arr=[1,2,3]
+// function sum(a,b,c){
+//     return a+b+c;
+// }
+//
+// console.log(sum(...arr))
+//
+// function res(arg,arg2,...REST){
+//     console.log(arg,arg2, REST);
+// }
+// res(1,2,3,4,5,6,7)
 
-const A={
-    a:'default',
-    b:'just test'
-}
-const B={
-    a:'value from b'
-}
-let C={
-    ...A,
-    ...B
-}
-console.log(C);
-const arr=[1,2,3]
-function sum(a,b,c){
-    return a+b+c;
-}
+// arrow Functions
+// const greet=(name='Zhora',age=3)=>`Hello ${name},your age is ${age}`
+// console.log(greet("ametist",35))
+//
+// const f=(a,b)=>{
+//     console.log(a+b);
+// }
+// f(1,2)
 
-console.log(sum(...arr))
 
-function res(arg,arg2,...REST){
-    console.log(arg,arg2, REST);
-}
-res(1,2,3,4,5,6,7)
+//for of
+// const names=['Jack','Max','Leo'];
+// // for(let index in names){
+// //     console.log(names[index]);
+// // }
+// for(let name of names){
+//     console.log(name);
+// }
 
-const greet=(name='Zhora',age=3)=>`Hello ${name},your age is ${age}`
-console.log(greet("ametist",35))
+//objects
 
-const f=(a,b)=>{
-    console.log(a+b);
-}
-f(1,2)
 
